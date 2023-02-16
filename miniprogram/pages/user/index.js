d2f17237f9c5aed41e377d585dd782e0f0d92c6b
liuhanchuan <1005293916@qq.com>
2023-02-16 23:01:06 +0800

    修改用户中心

--- miniprogram/pages/user/index.js
+++ miniprogram/pages/user/index.js
@@ -1,5 +1,6 @@
 Page({
   data: {
+    img1: 'https://tdesign.gtimg.com/miniprogram/images/example1.png',
     currAuthStep: 1,
     userInfo: {
       avatarUrl: '',
@@ -7,42 +8,51 @@ Page({
       phoneNumber: '',
     },
   },
-
   onLoad() {
     this.init()
   },
-
-  onShow() {
-    
-  },
-
+  onShow() {},
   init() {
-    this.fetUseriInfoHandle();
+    this.getScopeInfo();
   },
-
-  fetUseriInfoHandle() {
+  getUserInfo() {
+    const _that = this
+    wx.getUserInfo({
+        success: function(res) {
+            _that.setData({
+                userInfo: {
+                    avatarUrl: res.userInfo.avatarUrl,
+                    nickName: res.userInfo.nickName,
+                    gender: res.userInfo.gender,
+                    phoneNumber: res.userInfo.phoneNumber || ''
+                },
+                currAuthStep: 2,
+            })
+        }
+    })
+  },
+  getScopeInfo() {
+    const _that = this
     wx.getSetting({
       success (res){
-        if (res.authSetting['scope.userLocation']) {
+        if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success: function(res) {
-              console.log(res.userInfo)
-            }
-          })
+          _that.getUserInfo()
+        } else {
+            wx.authorize({
+                scope: 'scope.userInfo',
+                success () {
+                    _that.getUserInfo()
+                }
+            })
         }
       }
     })
   },
-
-
-
   gotoUserEditPage() {
     const { currAuthStep } = this.data;
-    if (currAuthStep === 2) {
-      wx.navigateTo({ url: '/pages/usercenter/person-info/index' });
-    } else {
-      this.fetUseriInfoHandle();
+    if (currAuthStep === 1) {
+        this.getScopeInfo();
     }
   },
 

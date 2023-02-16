a4fb6ee9074157e7015dcad820c59c520cc58257
liuhanchuan <1005293916@qq.com>
2023-02-16 18:23:52 +0800

    添加tesign

---
+++ miniprogram/pages/user/components/user-center-card/index.js
@@ -0,0 +1,31 @@
+const AuthStepType = {
+  ONE: 1,
+  TWO: 2,
+};
+
+Component({
+  options: {
+    multipleSlots: true,
+  },
+  properties: {
+    currAuthStep: {
+      type: Number,
+      value: AuthStepType.ONE,
+    },
+    userInfo: {
+      type: Object,
+      value: {},
+    },
+  },
+  
+  data: {
+    defaultAvatarUrl:
+      'https://cdn-we-retail.ym.tencent.com/miniapp/usercenter/icon-user-center-avatar@2x.png',
+    AuthStepType,
+  },
+  methods: {
+    gotoUserEditPage() {
+      this.triggerEvent('gotoUserEditPage');
+    },
+  },
+});
--- miniprogram/pages/user/index.js
+++ miniprogram/pages/user/index.js
@@ -1,5 +1,49 @@
 Page({
-    data: {
-      
+  data: {
+    currAuthStep: 1,
+    userInfo: {
+      avatarUrl: '',
+      nickName: '正在登录...',
+      phoneNumber: '',
     },
-  });
+  },
+
+  onLoad() {
+    this.init()
+  },
+
+  onShow() {
+    
+  },
+
+  init() {
+    this.fetUseriInfoHandle();
+  },
+
+  fetUseriInfoHandle() {
+    wx.getSetting({
+      success (res){
+        if (res.authSetting['scope.userLocation']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function(res) {
+              console.log(res.userInfo)
+            }
+          })
+        }
+      }
+    })
+  },
+
+
+
+  gotoUserEditPage() {
+    const { currAuthStep } = this.data;
+    if (currAuthStep === 2) {
+      wx.navigateTo({ url: '/pages/usercenter/person-info/index' });
+    } else {
+      this.fetUseriInfoHandle();
+    }
+  },
+
+});
